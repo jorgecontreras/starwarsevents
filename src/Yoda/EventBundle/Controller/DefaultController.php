24f338c1026911fb2a6831afa157c0f6d0d3fb0e
Jorge Contreras <jorge_rafaelc@hotmail.com>
2015-01-24 21:59:51 -0500

    new Event

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($count, $firstName)
     {
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $name));
+    	
+    	//$em = $this->container->get('doctrine')->getManager();
+    	$em = $this->getDoctrine()->getManager();
+    	
+    	$repo = $em->getRepository('EventBundle:Event');
+    	
+    	$event = $repo->findOneBy(array(
+    			'name' => 'Darth\'s surprise birthday party!'
+    	));
+    	    	
+    	  	return $this->render(
+    			'EventBundle:Default:index.html.twig',
+    			array('name' => $firstName, 'count' => $count, 'event' => $event)
+    			);
+    	
     }
 }
