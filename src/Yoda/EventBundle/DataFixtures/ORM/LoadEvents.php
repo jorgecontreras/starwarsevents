f663db87efcf0ce2dfddebd246ae7efb15576483
Jorge Contreras <jorge_rafaelc@hotmail.com>
2015-02-02 20:55:09 -0500

    Form creation
    Adding custom forms

---
+++ src/Yoda/EventBundle/DataFixtures/ORM/LoadEvents.php
@@ -0,0 +1,33 @@
+<?php
+// src/Acme/HelloBundle/DataFixtures/ORM/LoadUserData.php
+
+namespace Yoda\EventBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Yoda\EventBundle\Entity\Event;
+
+class LoadEvents implements FixtureInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function load(ObjectManager $manager)
+	{
+		$event1 = new Event();
+		$event1->setName('Darth\'s surprise birthday party!');
+		$event1->setLocation('Deathstar');
+		$event1->setTime(new \DateTime('tomorrow noon'));
+		$event1->setDetails('Ha! Darth HATES surprises!!!!');
+		$manager->persist($event1);
+		
+		$event2 = new Event();
+		$event2->setName('Rebellion Fundaraiser Bake sale!');
+		$event2->setLocation('Endor');
+		$event2->setTime(new \DateTime('thursday noon'));
+		$event2->setDetails('Ewok pies! support the rebellion');
+		$manager->persist($event2);
+		
+		$manager->flush();
+	}
+}
